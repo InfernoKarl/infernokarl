9f31a489c8277985cc5796887d3412d5d662b1f8
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2023-04-19 18:29:00 -0700

    woosh

--- loadstring.lua
+++ loadstring.lua
@@ -1,9 +1,9 @@
 local placeId = game.PlaceId
 
-if placeId == 11769051406 then
+if placeId == 11769051406 then -- RPG Elevator
     loadstring(game:HttpGet("https://gist.githubusercontent.com/InfernoKarl/50466fbbbf04f08efd0aac280900bee6/raw/310087ed73b3299a65f222266027105d3c3338f0/infernorpghub.lua", true))()
-elseif placeId == 12454097560 then
-    loadstring(game:HttpGet("https://gist.githubusercontent.com/InfernoKarl/7f628a7750c82999b94dedc3345543e4/raw/c32065a8c021ac18caf1219d4704c0518c8976f9/soulcultivationgui.lua", true))()
+elseif placeId == 12454097560 then -- Soul Cultivation
+    loadstring(game:HttpGet("https://gist.githubusercontent.com/InfernoKarl/7f628a7750c82999b94dedc3345543e4/raw/553dbdfdca360f8b6d5453833721fd1e6dc04274/soulcultivationgui.lua", true))()
 else
     print("Invalid PlaceID")
 end
