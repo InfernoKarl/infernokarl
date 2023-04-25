f4eacff125f3b5be70aba97c87746b95d299097c
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2023-04-24 18:23:38 -0700

    Update Heroes Battlegrounds

--- loadstring.lua
+++ loadstring.lua
@@ -7,7 +7,7 @@ elseif placeId == 12454097560 then -- Soul Cultivation
 elseif placeId == 6986072639 then -- Skyrift
     loadstring(game:HttpGet("https://gist.githubusercontent.com/InfernoKarl/c1e850cdefbd449a506f1d350dfdc7d2/raw/68c54d39b16cc92e55f67c734e9ced456af88f90/skyriftgui", true))()
 elseif placeId == 13076380114 then -- Heroes Battlegrounds
-    loadstring(game:HttpGet("https://gist.githubusercontent.com/InfernoKarl/b228a6ae9d3280856e9ce695ca141ec8/raw/a5fd26e6aa5c0d76a3c10c247f6c8931675d6f5f/heroesbattlegroundsgui", true))()
+    loadstring(game:HttpGet("https://gist.githubusercontent.com/InfernoKarl/b228a6ae9d3280856e9ce695ca141ec8/raw/3af94ac33211af95cd055664223c249317d0eb00/heroesbattlegroundsgui.lua", true))()
 else
     print("Invalid PlaceID")
 end
